6df4e0a54a034a61991e9cbbb288877114a006c0
habiba03 <habibamohammedyahia@gmail.com>
2026-01-13 16:39:55 +0200

    exam total score

--- src/app/interfaces/iexam.ts
+++ src/app/interfaces/iexam.ts
@@ -4,6 +4,7 @@ export interface IexamContentData {
   "examTitle": "string",
   "examDescription": "string",
   "examDuration": "string",
+  "totalScore": number,
   "easy": number,
   "medium": number,
   "hard": number,
